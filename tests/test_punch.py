b0576468f674a6205249336890bbde86823b24f1
51stRoR <51sttestinrror@gmail.com>
2020-07-17 23:22:49 +0300

    test

--- tests/test_punch.py
+++ tests/test_punch.py
@@ -12,14 +12,14 @@ class TestPunch():
         assert time_page.is_empty_record_displayed() is True, "Empty records message not displayed"
         assert time_page.get_empty_record_msg() == "No attendance records to display", "Message incorrect"
 
-    # @pytest.mark.skip
+    @pytest.mark.skip
     def test_create_punch_record(self, email, password):
         time_page = self.login_page.correct_login(email, password).to_time_module()
         time_page.create_punch_record(self.punch_for_test)
         assert time_page.is_success_save_msg_displayed() is True, "Punch success save message not displayed"
         assert time_page.get_success_save_msg_txt() == "Successfully Saved", "Message incorrect"
 
-    # @pytest.mark.skip
+    @pytest.mark.skip
     def test_punch_record_in_list(self, email, password):
         time_page = self.login_page.correct_login(email, password).to_time_module()
         time_page.get_punch_data_from_records(self.punch_for_test)
